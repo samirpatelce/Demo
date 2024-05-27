d17369d07f943dcaf475999e5a2ed41ed17bb9dc
Samir Patel <samir.p@tridhya.com>
2024-05-27 08:08:35 +0530

    Java OOPS

---
+++ src/main/java/org/example/CoreJava.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class CoreJava {
+    public static void main(String[] args) {
+//        int i=1;
+        for (int i=1; i<5; i++)
+        {
+            System.out.println(i);
+//            i++;
+        }
+    }
+}
