ab1482ad70a8b451ff3dfd76a07ecd62ae2f0ad1
Samir Patel <samir.p@tridhya.com>
2024-07-01 18:37:48 +0530

    sec14

---
+++ src/main/java/Section14/Test.java
@@ -0,0 +1,13 @@
+package Section14;
+
+import java.util.ArrayList;
+
+public class Test {
+    public void regular(){
+        ArrayList<String> name = new ArrayList<String>();
+        name.add("Abhi");
+        name.add("axay");
+        name.add("sam");
+        name.add("aplo");
+    }
+}
