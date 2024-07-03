5be5ece0e05d8dedcce8f5fae51e509fa66c07ee
Samir Patel <samir.p@tridhya.com>
2024-07-03 17:46:14 +0530

    add example

--- src/main/java/Section14/Test.java
+++ src/main/java/Section14/Test.java
@@ -3,11 +3,35 @@ package Section14;
 import java.util.ArrayList;
 
 public class Test {
+    @org.testng.annotations.Test
     public void regular(){
         ArrayList<String> name = new ArrayList<String>();
         name.add("Abhi");
-        name.add("axay");
+        name.add("Axay");
         name.add("sam");
         name.add("aplo");
+        int count = 0;
+
+        for (int i=0; i<name.size();i++){
+            String startedName = name.get(i);
+            if(startedName.startsWith("A")){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    @org.testng.annotations.Test
+    public void steramFilter(){
+        ArrayList<String> name = new ArrayList<String>();
+        name.add("Abhi");
+        name.add("Axay");
+        name.add("sAam");
+        name.add("Aaplo");
+
+        Long c = name.stream().filter(s->s.startsWith("A")).count();
+        System.out.println(c);
+
+
     }
 }
---
+++ src/main/java/org/example/Amazon.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Amazon {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.in/");
+        String name = "Storite Dust & Rain Cover for Backpack with Pouch, Waterproof Dustproof Rain Cover Bag Elastic Adjustable for School, College,Office, Trekking Bags (30L-35L,Black)";
+        driver.findElement(By.xpath("//div[@id='nav-xshop']//a[contains(.,'Best Sellers')]")).click();
+        List<WebElement> productName = driver.findElements(By.xpath("//div[@class='p13n-sc-truncate-desktop-type2  p13n-sc-truncated']"));
+
+        for (WebElement webElement : productName) {
+            String name12 = webElement.getAttribute("title");
+            if (name12.contains(name)) {
+                driver.findElement(By.xpath("//div[@title='" + name + "']")).click();
+                driver.findElement(By.xpath("//span[@class='a-button-inner']//input[@id='add-to-cart-button']")).click();
+            }
+        }
+    }
+}
